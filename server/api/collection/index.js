55d3c42c2017c6da3d19ad5359c4fdb98f316764
Patrick San Juan <psj.webdev@gmail.com>
2017-02-02 16:22:19 -0800

    api test drafted get releases of loggedin user

--- server/api/collection/index.js
+++ server/api/collection/index.js
@@ -8,8 +8,25 @@ var router = express.Router();
 
 router.get('/me', function(req, res) {
   var dis = new Discogs(req.session.accessData);
+  var col = new Discogs().user().collection();
+  var releases = [];
   dis.getIdentity(function(err, data){
-      console.log(data);
+  	console.log(data.username);
+  	col.getReleases('mike800gie'/* replace with data.username */, 
+  		0, { page: 1, per_page: 25 }, function(err, data){
+  			data.releases.forEach(function(release, idx) {
+  				releases.push({
+  					title: release.basic_information.title,
+  					artist: release.basic_information.artists[0].name,
+  					label: release.basic_information.labels[0].name,
+  					id: release.id
+  				})
+  			});
+  			data['folder'] = 0;
+  			data['releases'] = releases;
+  			console.log(data);
+  			res.json(data);
+  	});
   });
 });
 
