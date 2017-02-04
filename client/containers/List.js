0285c569ef94e39c1c38f7671664c96d8726d380
Patrick San Juan <psj.webdev@gmail.com>
2017-02-03 17:33:01 -0800

    react only fetch request

--- client/containers/List.js
+++ client/containers/List.js
@@ -5,8 +5,22 @@ export default class List extends Component {
 
 	constructor(props) {
 		super(props);
+		this.state = { loading: false, releases: [] }
+	}
+
+	componentDidMount() {
+		this.setState({ loading: true });
 		fetch('/api/collection/me', { credentials: 'same-origin' })
-			.then(res => console.log('complete! - ', res))
+			.then(res => res.json())
+			.then(json => {
+				console.log(json);
+				this.setState({
+					folder: json.folder,
+					pagination: json.pagination,
+					releases: json.releases,
+					loading: false 
+				});
+			})
 			.catch(err => console.error(err));
 	}
 
@@ -14,6 +28,9 @@ export default class List extends Component {
 		return (
 			<div>
 				<h2>Rec-list here...</h2>
+				{this.state.releases.map((release, idx) =>
+					<div key={idx}>{release.title} - {release.artist}</div>
+				)}
 			</div>
 		)
 	}
